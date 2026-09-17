96ea0ea0b41123d3cb8cda18182ec645004be34e
Lívia Andrade <lrandrade06@gmail.com>
2026-09-16 22:05:02 -0300

    Home/cards notícias

--- src/app/features/public/pages/home/home.component.ts
+++ src/app/features/public/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../../../component/banner/banner.component';
 import { FooterComponent } from '../../../../component/footer/footer.component';
+import { CardNewsComponent } from '../../../../component/card-news/card-news.component';
 
 @Component({
   selector: 'app-home',
-  imports: [BannerComponent, FooterComponent],
+  imports: [BannerComponent, FooterComponent, CardNewsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
